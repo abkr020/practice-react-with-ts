d97c531821c98bc532eeb19f8181475bb1c2a9df
abkr020 <abkr.020@gmail.com>
2025-07-18 23:08:43 +0530

    next structre chagen

--- src/pages/fileExplorer/FileExplorer.tsx
+++ src/pages/fileExplorer/FileExplorer.tsx
@@ -23,12 +23,26 @@ const FileExplorerPage = () => {
                         ]
                     }
                 ]
+            },
+            {
+                name:'folder 0.5'
             }
         ]
     })
     return (
         <>
             <h1>file explorer</h1>
+            {Object.entries(structure).map(([key, value]) => {
+                console.log(key);
+                console.log(typeof (key));
+                console.log(typeof (structure[key]));
+                
+                // if (typeof (structure[key]) === 'string') {
+                //     console.log(key);
+                //    return <div>folder {structure[key]}</div>
+                // }
+                // if(typeof(structure[key]))
+            })}
         </>
     )
 }
