42fd2a763c89ac3c1c0e2f46ec95b989200693d7
abkr020 <abkr.020@gmail.com>
2025-07-28 23:00:18 +0530

    next mouse leave

--- src/components/svgString/SvgString.tsx
+++ src/components/svgString/SvgString.tsx
@@ -1,10 +1,27 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const SvgString = () => {
-    const [val ,setVal] = useState('M 100 200 Q 550 200 1000 200')
+    const [val, setVal] = useState('M 100 200 Q 550 200 1000 200')
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        const x = e.clientX;
+        // const y = e.clientY;
+        // const newVal = `M 100 200 Q 550 ${y} 1000 200`;
+        if (containerRef.current) {
+
+            const rect = containerRef.current.getBoundingClientRect();
+            const y = e.clientY - rect.top; // y relative to the container
+            const newVal = `M 100 200 Q 550 ${y} 1000 200`;
+            setVal(newVal);
+        }
+    }
     return (
-        <div style={{ backgroundColor: 'darkgray', height:'50vh', padding:'20px'}}>
-            <svg height='100%' width='100%' style={{backgroundColor:'black' }} >
+        <div style={{ backgroundColor: 'darkgray', height: '50vh', padding: '20px' }}
+            onMouseMove={handleMouseMove}
+            ref={containerRef}
+        >
+            <svg height='100%' width='100%' style={{ backgroundColor: 'black' }} >
                 <path d={val} stroke='white' />
             </svg>
         </div>
